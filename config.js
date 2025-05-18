3a328a80a2caf42b95be00ea665fca8c4a069df6
悪 𝐊𝚫𝐋𝚰𝐘𝚫 𝚾 悪 <kaliya.x.git@gmail.com>
2025-05-18 09:44:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // USE ENVIRONMENT VARIABLES FOR EMAIL AND PASSWORD INSTEAD.
 module.exports = {
-  PREFIX: process.env.PREFIX || "IRON-M4N×",
-  EMAIL: process.env.EMAIL || "ironman@onlyfans.com",
-  PASS: process.env.PASS || "Kimi da yo kimi nandayo..",
+  PREFIX: process.env.PREFIX || "kaliya×",
+  EMAIL: process.env.EMAIL || "kaliya.x.git@gmail.com",
+  PASS: process.env.PASS || "#kalibbh12",
   MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id blah blah .etc",
   IMAGE: process.env.IMAGE || "https://cdn.ironman.my.id/i/2iceb4.jpeg",
 }
